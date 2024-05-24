1ac50711d89a962c541fb8e0ab479734cb279527
Hepheir <hepheir@gmail.com>
2024-05-24 15:14:06 +0900

    feat: username 으로 필터링 할 수 있는 기능 추가

--- otoeic/user/views.py
+++ otoeic/user/views.py
@@ -8,3 +8,15 @@ class UserRestAPI(generics.ListCreateAPIView):
     queryset = UserDAO.objects.all()
     serializer_class = UserSerializer
     permission_classes = []
+
+    def get_queryset(self):
+        """
+        This view should return a list of all the purchases for
+        the user as determined by the username portion of the URL.
+        """
+        queryset = super().get_queryset()
+        if self.request.query_params:
+            username = self.request.query_params.get('username')
+            print(username)
+            return queryset.filter(username=username)
+        return queryset
