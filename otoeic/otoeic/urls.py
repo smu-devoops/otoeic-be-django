5be70c4cb1ab4a0e6ae06448df1763a89d3b837c
Hepheir <hepheir@gmail.com>
2024-05-24 20:51:52 +0900

    chore: add STATIC_ROOT

--- otoeic/otoeic/urls.py
+++ otoeic/otoeic/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
@@ -23,4 +25,4 @@ urlpatterns = [
     path('', include('word.urls')),
     path('', include('exam.urls')),
     path("admin/", admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
