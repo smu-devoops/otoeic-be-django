6e5c935e9a7b4f8465727e9157ebf0e72a715b20
Hepheir <hepheir@gmail.com>
2024-05-24 18:21:34 +0900

    feat(exam): ExamDAO, ExamQuestionDAO 모델 추가

--- otoeic/exam/models.py
+++ otoeic/exam/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.core.validators import MaxValueValidator
 
-# Create your models here.
+from user.models import Level
+from user.models import UserDAO
+from word.models import WordDAO
+
+
+class ExamDAO(models.Model):
+    id = models.IntegerField(primary_key=True, auto_created=True)
+    user = models.ForeignKey(UserDAO, on_delete=models.CASCADE)
+    level = models.IntegerField(choices=Level.choices)
+    amount = models.IntegerField(validators=[MinValueValidator(10), MaxValueValidator(40)])
+    point = models.IntegerField(default=0)
+    date_created = models.DateTimeField(auto_now=True)
+    date_submitted = models.DateTimeField(null=True)
+
+
+class ExamQuestionDAO(models.Model):
+    exam = models.ForeignKey(ExamDAO, on_delete=models.CASCADE)
+    word = models.ForeignKey(WordDAO, on_delete=models.PROTECT)
+    order = models.IntegerField()
+    submitted_answer = models.TextField(blank=True, default='')
