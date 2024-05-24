b283d6165ccf8505f4a1ddcb256af5c14b7e2647
Hepheir <hepheir@gmail.com>
2024-05-25 01:16:25 +0900

    refactor: revert "change to PRODUCTION mode"
    This reverts commit 4f2d9cc94a2620e50a8522d690748aea2223c511.

--- otoeic/otoeic/settings.py
+++ otoeic/otoeic/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-5)vg3itd%!grru6gqsg2&n_z)iqwnmi=vx40d-0sm9cak)0dk^")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
