e9ec0b0f8b2848f0d40ce29837e98f1954baf75f
Hepheir <hepheir@gmail.com>
2024-05-29 18:46:37 +0900

    fix(user): user password가 unique 이면 안되죠..

--- otoeic/user/models.py
+++ otoeic/user/models.py
@@ -15,7 +15,7 @@ class UserDAO(AbstractUser):
     id = models.BigAutoField(primary_key=True, auto_created=True)
     email = models.EmailField(null=True, default=None) # 사용 안 함.
     username = models.TextField(unique=True, null=False, blank=False)
-    password = models.TextField(unique=True, null=False, blank=False)
+    password = models.TextField(null=False, blank=False)
     level = models.IntegerField(choices=Level.choices, default=Level.LEVEL_1)
     streak = models.IntegerField(default=0)
     streak_freeze_amount = models.IntegerField(default=0)
