d178412b0db8401f3389721ca9d6cd90bf50e4b3
Hepheir <hepheir@gmail.com>
2024-05-29 23:07:24 +0900

    feat(user): create UsernameSerializer

--- otoeic/user/serializers.py
+++ otoeic/user/serializers.py
@@ -8,6 +8,19 @@ class UsernamePasswordSerializer(serializers.Serializer):
     password = serializers.CharField()
 
 
+class UsernameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserDAO
+        fields = [
+            'id',
+            'username',
+        ]
+        extra_kwargs = {
+            'id': {'read_only': True},
+            'username': {'read_only': True},
+        }
+
+
 class UserSerializer(serializers.ModelSerializer):
     is_admin = serializers.SerializerMethodField()
     streak = serializers.SerializerMethodField()
@@ -31,7 +44,7 @@ class UserSerializer(serializers.ModelSerializer):
         }
 
     def get_is_admin(self, obj: UserDAO) -> bool:
-        return obj.is_superuser
+        return obj.is_staff
 
     def get_streak(self, obj: UserDAO):
         # TODO
