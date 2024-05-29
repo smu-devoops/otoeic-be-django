65ff9aae6176a553c4144b0cbdc2531ea5a8a5e8
Hepheir <hepheir@gmail.com>
2024-05-30 03:56:09 +0900

    feat(exam): 시험 자동 생성 기능을 API 명세에 맞게 구현

--- otoeic/exam/serializers.py
+++ otoeic/exam/serializers.py
@@ -1,7 +1,60 @@
 from rest_framework import serializers
+from rest_framework.request import Request
 
+from user.models import UserDAO
+from user.serializers import UsernameSerializer
 from word.serializers import WordSerializer
+from word.serializers import WordForUnsubmittedExamSerializer
 from . import models
+from . import services
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    word = WordForUnsubmittedExamSerializer(read_only=True)
+
+    class Meta:
+        model = models.ExamQuestionDAO
+        fields = [
+            'word',
+            'order',
+        ]
+        extra_kwargs = {
+            'word': {'read_only': True},
+            'order': {'read_only': True},
+        }
+
+
+class UnsubmittedExamSerializer(serializers.ModelSerializer):
+    user_created = UsernameSerializer(read_only=True)
+    questions = QuestionSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = models.ExamDAO
+        fields = [
+            'id',
+            'level',
+            'amount',
+            'ranked',
+            'user_created',
+            'date_created',
+            'questions',
+        ]
+        extra_kwargs = {
+            'id': {'read_only': True},
+            'user_created': {'read_only': True},
+            'date_created': {'read_only': True},
+            'questions': {'read_only': True},
+        }
+
+    def create(self, validated_data):
+        request: Request = self.context.get('request')
+        assert request.user.is_authenticated, (
+            f'Could not find user {request.user}.'
+        )
+        validated_data['user_created'] = request.user
+        exam: models.ExamDAO = super().create(validated_data)
+        services.create_questions(exam, shuffle=True)
+        return exam
 
 
 class ExamQuestionSerializer(serializers.ModelSerializer):
---
+++ otoeic/exam/services.py
@@ -0,0 +1,50 @@
+import random
+import typing
+
+from django.db.transaction import atomic
+
+from word.models import WordDAO
+from . import models
+
+
+DAILY_BONUS_POINT_MULTIPLIER = 3
+
+
+def create_questions(exam: models.ExamDAO, shuffle=True):
+    assert exam.questions.count() == 0, (
+        f'This exam already have {exam.questions.count()} questions.'
+    )
+    words = list(WordDAO.objects.filter(level=exam.level))
+    if shuffle:
+        random.shuffle(words)
+    words = words[:exam.amount]
+    with atomic():
+        for order, word in enumerate(words, start=1):
+            question = models.ExamQuestionDAO.objects.create(
+                exam=exam,
+                word=word,
+                order=order,
+            )
+            question.full_clean()
+            question.save()
+
+
+def create_result(exam: models.ExamDAO, answers: typing.List[str]):
+    questions = exam.questions.order_by(models.ExamQuestionDAO.order)
+    assert len(questions) == len(answers), (
+        f'Number of answers({len(answers)}) does not match with number of questions({len(questions)}).'
+    )
+    with atomic():
+        for question, answer in zip(questions, answers):
+            question.answer_submitted = answer
+            question.is_correct = bool(question.word.english == answer)
+            question.full_clean()
+            question.save()
+        # TODO: give more points for today's first exam
+        result = models.ExamResultDAO.objects.create(
+            exam=exam,
+            points=exam.questions.filter(is_correct=True).count(),
+        )
+        # TODO: 사용자 수준 점검 관련 기능 추가
+        result.full_clean()
+        result.save()
