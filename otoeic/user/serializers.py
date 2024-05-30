7af024f497513e3a109f4c37f564ef0e64496c01
Hepheir <hepheir@gmail.com>
2024-05-30 16:52:15 +0900

    fix(user): UserSerialier 수정

--- otoeic/user/serializers.py
+++ otoeic/user/serializers.py
@@ -45,30 +45,18 @@ class UsernameSerializer(serializers.ModelSerializer):
 
 
 class UserSerializer(serializers.ModelSerializer):
-    is_admin = serializers.SerializerMethodField()
-    streak = serializers.SerializerMethodField()
-
     class Meta:
         model = models.UserDAO
         fields = [
             'id',
             'username',
-            'is_admin',
             'level',
-            'streak',
             'streak_freeze_amount',
             'is_streak_freeze_activated',
-            'point'
+            'point',
+            'is_staff',
         ]
         extra_kwargs = {
             'id': {'read_only': True},
-            'email': {'write_only': True},
-            'is_admin': {'read_only': True},
+            'is_staff': {'read_only': True},
         }
-
-    def get_is_admin(self, obj: UserDAO) -> bool:
-        return obj.is_staff
-
-    def get_streak(self, obj: UserDAO):
-        # TODO
-        return 0
